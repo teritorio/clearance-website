138a426fbd30939f4ed23e2872f90f9b53ce7632
Noé Viricel <noe.viricel@wazo-lab.io>
2026-07-03 04:00:15 +0700

    feat(analytics): disable Matomo tracking in development

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,22 +11,6 @@ export default defineNuxtConfig({
 
   app: {
     head: {
-      script: [
-        {
-          innerHTML: `
-            var _paq = window._paq = window._paq || [];
-            _paq.push(['trackPageView']);
-            _paq.push(['enableLinkTracking']);
-            (function() {
-              var u="https://matomo.teritorio.xyz/";
-              _paq.push(['setTrackerUrl', u+'matomo.php']);
-              _paq.push(['setSiteId', '32']);
-              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-              g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-            })();
-          `,
-        },
-      ],
       link: [
         { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
       ],
